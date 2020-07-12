1812538a5f02d56dcfc9dc6cdf39f0bf4d5c263d
chencl1986 <chencl1986@qq.com>
2020-07-12 11:41:37 +0800

    feat(): 提交打包后文件

---
+++ build/precache-manifest.f81c2a098430080b914ef720d8f665f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a32588554262f3b59801f216e33ed10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e73c8e074528e049c79",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "b47c1fbcbdaeca185230",
+    "url": "/static/css/main.bd8c6a8d.chunk.css"
+  },
+  {
+    "revision": "1e73c8e074528e049c79",
+    "url": "/static/js/2.dc643696.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/static/js/2.dc643696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47c1fbcbdaeca185230",
+    "url": "/static/js/main.6adcbaff.chunk.js"
+  },
+  {
+    "revision": "2c642597106ff16b5761",
+    "url": "/static/js/runtime-main.24858535.js"
+  }
+]);
